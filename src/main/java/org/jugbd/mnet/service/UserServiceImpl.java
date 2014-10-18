92b5506c1cdd5484a9751745e77cbf838c37f25d
A. N. M. Bazlur Rahman <anm_brr@yahoo.com>
2014-10-19 02:35:19 +0600

    Fix password salt 8 character to 16 character

--- src/main/java/org/jugbd/mnet/service/UserServiceImpl.java
+++ src/main/java/org/jugbd/mnet/service/UserServiceImpl.java
@@ -34,7 +34,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         user.setUsername(user.getUsername().trim());
-        user.setSalt(StringUtils.generateRandomString(8));
+        user.setSalt(StringUtils.generateRandomString(16));
         user.setHashedPassword(messageDigestPasswordEncoder.encodePassword(user.getPassword(), user.getSalt()));
         user.setEnabled(true);
         user.setAccountNonExpired(true);
